dffe2192fc659e12b6ef3798ee3058cc6a419e49
mrnibbsy <59174435+mrnibbsy@users.noreply.github.com>
2025-05-12 08:54:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 const upload = multer({ storage: multer.memoryStorage() });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Supabase-Konfiguration
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
